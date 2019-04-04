5a0574ec01b1d241379843438a9648b4ed7d3252
ryanboris <ryan.boris@gmail.com>
2019-04-03 23:37:17 -0400

    completes demo

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <h1>About me</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit similique ab
+      cumque nulla veritatis!
+    </p>
+  </Layout>
+)
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, delectus?
+    </h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+  </Layout>
+)
